c1c738b063b03f5f780a8c30e7f0e366d05d2a5e
Kukukun <theophile.varnier@gmail.com>
2016-03-30 21:44:17 +0200

    toto

--- LeFrenchMelee.Web/Scripts/tournamentCreate.js
+++ LeFrenchMelee.Web/Scripts/tournamentCreate.js
@@ -4,22 +4,21 @@
     $("#accordion").accordion();
 
     $("#additionalFields").on("click", ".add-field", function (e, obj) {
-        $("#additionalFields").append('<div class="form-group">'
-            + '<div class="col-xs-2">'
-            + '<input type="text" class="form-control" placeholder="Nom du champ..." />'
-            + '</div>'
-            + '<div class="col-xs-3">'
-            + '<div class="input-group">'
-            + '<select class="form-control">'
-            + '<option value="" disabled selected>Type de champ</option>'
-            + '<option value="Texte">Texte</option>'
-            + '<option value="Nombre">Nombre</option>'
-            + '</select>'
-            + '<span class="input-group-btn">'
-            + '<button type="button" class="btn btn-primary add-field"><span class="glyphicon glyphicon-plus"></span></button>'
-            + '</span>'
-            + '</div>'
-            + '</div>'
-            + '</div>');
+        if ($(this).hasClass("btn-primary")) {
+            var content = $("#templateField").html();
+            //var regex = "/AdditionalFields([^\d]+)0([^\d\.]+)/gm";
+            var regex = new RegExp("AdditionalFields([^\d]+)0([^\d\.\"]+)", "gm");
+            //$("#additionalFields").append(_.replace(content, regex, function ($0, $1, $2) {
+            //$("#additionalFields").append(content.replace(regex, function ($0, $1, $2) {
+            //    return 'AdditionalFields' + $1 + $(".additional-field").length + $2;
+            //}));
+            var newString = content.replace(regex, "AdditionalFields$1" + ($(".additional-field").length - 1) + "$2");
+            $("#additionalFields").append(newString);
+            $(this).removeClass("btn-primary")
+                .addClass("btn-danger")
+                .find(".glyphicon-plus").removeClass(".glyphicon-plus").addClass("glyphicon-minus");
+        } else {
+            $(this).closest(".additional-field").remove();
+        }
     });
 });
